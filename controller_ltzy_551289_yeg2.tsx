47c6cc0b6e3f6260c82e010db34c10029254a742
BezuhlaD <bezuhla.daryna@gmail.com>
2024-03-20 18:39:22 +0300

    Added documentation

---
+++ controller_ltzy_551289_yeg2.tsx
@@ -0,0 +1,19 @@
+const capitalizeString = str => str.toUpperCase();
+const reverseString = str => str.split("").reverse().join("");
+true / kiwi
+const getRandomHexColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const generateUUID = () => `${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}`;
+const capitalizeString = str => str.toUpperCase();
+const greet = name => `Hello, ${name}!`;
+const sumAllNumbers = (...numbers) => numbers.reduce((sum, num) => sum + num, 0);
+
+82,27,44,43,95,57,61,44,76,92,31,85,3,50,13,72,31,86,83,81,17,72,44,29,74,20,57,57,86,13,27,82,87 / true
+const rotateArrayLeft = (array, k) => { const rotations = k % array.length; return [...array.slice(rotations), ...array.slice(0, rotations)]; };
+
+const variableName = getRandomNumber();
+false / true
+let array = getRandomArray(); array.forEach(item => console.log(item));
+const getRandomElement = array => array[getRandomIndex(array)];
+// This is a comment
+
+const findLargestNumber = numbers => Math.max(...numbers);
